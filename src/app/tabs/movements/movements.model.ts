3091c68ef301b97c72babc6638b330586a4f6004
Bernardo Laing <bernardo.laing@gmail.com>
2020-02-16 16:02:16 -0600

    Add movement models

---
+++ src/app/tabs/movements/movements.model.ts
@@ -0,0 +1,23 @@
+import { DocumentReference } from 'angularfire2/firestore';
+
+export class Movement {
+  id?: string;
+  date: Date;
+  amount: number;
+  description: string;
+}
+
+export class Expense extends Movement {
+  category: DocumentReference;
+  account: DocumentReference;
+  isBilled: boolean;
+}
+
+export class Income extends Movement {
+  account: DocumentReference;
+}
+
+export class Exchange extends Movement {
+  sourceAccount: DocumentReference;
+  destinationAccount: DocumentReference;
+}
