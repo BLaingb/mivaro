e5378a8d8b7bfd40cb5ec97be9f756e9b5750205
Bernardo Laing <bernardo.laing@gmail.com>
2020-02-21 19:54:20 -0600

    Add name field to account

--- src/app/tabs/accounts/accounts.model.ts
+++ src/app/tabs/accounts/accounts.model.ts
@@ -3,6 +3,7 @@ import { DocumentReference } from '@angular/fire/firestore';
 export interface Account {
   user: DocumentReference;
   id?: string;
+  name: string;
   startingBalance: number;
   balance: number;
 }
