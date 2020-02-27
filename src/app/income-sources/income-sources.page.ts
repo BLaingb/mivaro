2e809584dbc34f97a74a2b767a095ae900e6b5dc
Bernardo Laing <bernardo.laing@gmail.com>
2020-02-27 15:20:39 -0600

    Add functionality to create new income source

--- src/app/income-sources/income-sources.page.ts
+++ src/app/income-sources/income-sources.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IncomeSource } from './income-source.model';
 import { IncomeSourcesService } from './income-sources.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-income-sources',
@@ -9,15 +10,28 @@ import { IncomeSourcesService } from './income-sources.service';
 })
 export class IncomeSourcesPage implements OnInit {
   sources: IncomeSource[];
+  form: FormGroup;
 
   constructor(
-    private incomeSourceService: IncomeSourcesService
+    private incomeSourceService: IncomeSourcesService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
     this.incomeSourceService.getListObservable().subscribe(srcs => {
       this.sources = srcs;
     });
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      plannedIncome: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onConfirm() {
+    if (this.form.valid) {
+      this.incomeSourceService.addDocument({ ...this.form.value });
+      this.form.reset();
+    }
   }
 
 }
